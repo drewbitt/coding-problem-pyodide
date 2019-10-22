228f19816cefd732ad33be65195126b3f34d728e
drewbitt <andrew@drewbitt.com>
2019-10-22 16:19:46 -0400

    Add pyodide implementation, local data vars

--- src/pages/New.vue
+++ src/pages/New.vue
@@ -4,19 +4,19 @@
       <div class="columns is-centered">
         <div>
           <b-field label="Name">
-            <b-input v-model="name" />
+            <b-input v-model="nameL" />
           </b-field>
           <b-field label="Input">
-            <b-input v-model="input" />
+            <b-input v-model="inputL" />
           </b-field>
           <b-field label="Code">
-            <codemirror v-model="code" class="codemirror" />
+            <codemirror v-model="codeL" class="codemirror" />
           </b-field>
           <b-field label="Output">
-            <b-input v-model="output" />
+            <textarea v-model="outputL" readonly rows="4" />
           </b-field>
           <b-button type="button is-primary" @click="updateList">Save</b-button>
-          <b-button type="button" outlined>Run</b-button>
+          <b-button type="button" @click="run()" outlined>Run</b-button>
         </div>
       </div>
     </section>
@@ -25,6 +25,7 @@
 
 <script>
 import { codemirror } from "vue-codemirror";
+import { plugin } from "../pyodide/pyodide";
 import "codemirror/lib/codemirror.css";
 import "codemirror/mode/python/python.js";
 
@@ -37,6 +38,14 @@ export default {
     output: String,
     isNew: { type: Boolean, default: true },
     id: Number
+  },
+  data: function () {
+    return {
+      inputL: this.input,
+      nameL: this.name,
+      codeL: this.code,
+      outputL: this.output
+    }
   },
   components: {
     codemirror
@@ -78,6 +87,15 @@ export default {
           position: "is-bottom"
         });
       }
+    },
+    run() {
+      let outputPyodide = null;
+
+      plugin().then(() => {
+        pyodide.loadPackage(['numpy']).then(() => {
+                    this.outputL = pyodide.runPython(this.codeL);
+                });
+      });
     }
   }
 };
