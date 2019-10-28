20ff94879aace1b05cb9c13ec34bfa42745df3db
drewbitt <andrew@drewbitt.com>
2019-10-28 10:15:56 -0400

    Add padding

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -2,8 +2,8 @@
   <div class="columns is-centered">
       <!--- Using root data for now instead of state-->
       <ul>
-      <li v-for="item in this.$root.$data.items" :key="item.id">
-      <b-button expanded type="is-dark is-medium" >
+      <li v-for="item in this.$root.$data.items" :key="item.id" style="margin: 10px;">
+      <b-button expanded type="is-dark is-medium">
                 <router-link
           :to="{
             name: 'item',
